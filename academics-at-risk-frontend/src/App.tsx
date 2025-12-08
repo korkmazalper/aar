0657eec438221e614d7ee1516a21f1943a47bd59
korkmaz <dr.alperkorkmaz@gmail.com>
2025-12-08 22:43:55 +0100

    viteconfig fixed

--- academics-at-risk-frontend/src/App.tsx
+++ academics-at-risk-frontend/src/App.tsx
@@ -5,18 +5,16 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
+
 function App() {
   return (
-    <Router>
+    <Router basename="/aar">
       <div className="app">
         <Header />
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/contact" element={<Contact />} />
-            {
-
-            }
           </Routes>
         </main>
         <Footer />
--- academics-at-risk-frontend/vite.config.ts
+++ academics-at-risk-frontend/vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command }) => {
-  const base = command === "build" ? "/aar/" : "/";
-
-  return {
-    plugins: [react()],
-    base: base,
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: "/aar/",
 });
