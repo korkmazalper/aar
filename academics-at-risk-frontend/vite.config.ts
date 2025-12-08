12981d8edcb4290e4900605ebde8262c6d2e1eab
korkmaz <dr.alperkorkmaz@gmail.com>
2025-12-08 22:13:36 +0100

    viteconfig fixed

--- academics-at-risk-frontend/vite.config.ts
+++ academics-at-risk-frontend/vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/aar/",
+export default defineConfig(({ command }) => {
+  const base = command === "build" ? "/aar/" : "/";
+
+  return {
+    plugins: [react()],
+    base: base,
+  };
 });
